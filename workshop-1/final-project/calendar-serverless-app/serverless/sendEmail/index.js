51b2855cd3acb8f261a6834b735b401a572fc503
glaucia86 <glaucia_lemos86@hotmail.com>
2019-08-22 00:10:43 -0300

    Desenvolvimento da lógica do 'sendEmail'

--- workshop-1/final-project/calendar-serverless-app/serverless/sendEmail/index.js
+++ workshop-1/final-project/calendar-serverless-app/serverless/sendEmail/index.js
@@ -1,14 +1,17 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
+﻿const sgMail = require('@sendgrid/mail');
 
-module.exports = async function (context) {
-    return `Hello ${context.bindings.name}!`;
-};
+sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
+
+module.exports = async function(context) {    
+    const { email, title, startAt, description } = context.bindings.payload;
+    
+    const msg = {
+        to: email,
+        from: { email: 'skinclear86@gmail.com', name: 'Glaucia Lemos Calendar' },
+        subject: `Evento: ${title}`,
+        html: `<h4>${title} @ ${startAt}</h4> <p>${description}</p>`
+    };
+
+    sgMail.send(msg);
+    return msg;   
+}
